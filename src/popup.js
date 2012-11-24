bc145f5595f32c326443cd8ad59c1259b910a9d5
Claudio d'Angelis <claudiodangelis@gmail.com>
2012-11-24 17:02:51 +0100

    fixing page height issue

--- src/popup.js
+++ src/popup.js
@@ -5,65 +5,65 @@ chrome.tabs.getSelected(null, function(tab) {
 });
 
 var input = document.getElementById("filter");
-	input.onkeydown = (function(){
-		chrome.tabs.getSelected(null, function(tab) {
-		  chrome.tabs.sendMessage(tab.id, {action: "get_content"}, function(content) {
-		    filter(content,input);
-		  });
-		});
+  input.onkeydown = (function(){
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendMessage(tab.id, {action: "get_content"}, function(content) {
+        filter(content,input);
+      });
+    });
 });
 
 function showContent(content){
-	if (!content['null']){
-		for ( var key in content){
-			if ( key != 'null'){
-				// Create div
-				div = document.createElement("div");
-				div.setAttribute("class","resultBox");
+  if (!content['null']){
 
-				// Create <p> title
-				p = document.createElement("p");
-				p.innerHTML = content[key][1];
+    for ( var key in content){
 
-				element = document.createElement("a");
-				element.href=content[key][0];
-				element.innerHTML=key;
-				element.onclick = (function(url) {
-					return function(){
-						download(url);
-						};
-					})(content[key][0]);
+      if ( key != 'null'){
+        // Create div
+        div = document.createElement("div");
+        div.setAttribute("class","resultBox");
 
-				div.appendChild(p);
-				div.appendChild(element);
-				document.getElementById("content").appendChild(div);
+        // Create <p> title
+        p = document.createElement("p");
+        p.innerHTML = content[key][1];
 
-			}
-		}
-	}
-	 else {
-	document.getElementById("content").innerHTML = "<p>Nothing to show.</p>"
+        element = document.createElement("a");
+        element.href=content[key][0];
+        element.innerHTML=key;
+        element.onclick = (function(url) {
+          return function(){
+            download(url);
+            };
+          })(content[key][0]);
+
+        div.appendChild(p);
+        div.appendChild(element);
+        document.getElementById("content").appendChild(div);
+
+      }
+    }
+  }
+   else {
+  document.getElementById("content").innerHTML = "<p>Nothing to show.</p>"
 }
 }
 
 function download(url){
-	chrome.tabs.create({'url': url}, function(tab) {});}
+  chrome.tabs.create({'url': url}, function(tab) {});}
 
 function filter(content){
-	console.log(input.value);
-	console.log(content);
 
-	var filteredContent = new Object();
-	filteredContent['null'] = true;
+  var filteredContent = new Object();
+  filteredContent['null'] = true;
 
-	for ( var key in content ){
-		if ( key != 'null'){
-			if (key.toLowerCase().indexOf(input.value.toLowerCase())!=-1 || content[key][0].toLowerCase().indexOf(input.value.toLowerCase()) != -1 ){
-				filteredContent[key]=[content[key][0],content[key][1]];
-				filteredContent['null']=false;
-			}
-		}
-	}
-	document.getElementById("content").innerHTML="";
-	showContent(filteredContent);
+  for ( var key in content ){
+    if ( key != 'null'){
+      if (key.toLowerCase().indexOf(input.value.toLowerCase())!=-1 || content[key][0].toLowerCase().indexOf(input.value.toLowerCase()) != -1 ){
+        filteredContent[key]=[content[key][0],content[key][1]];
+        filteredContent['null']=false;
+      }
+    }
+  }
+  document.getElementById("content").innerHTML="";
+  showContent(filteredContent);
 }
